0a9e8c1852e832aebe00ca1fecb6affce6f5a42c
Charles Chin <eallion@eallion.com>
2019-12-07 23:39:57 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,15 +1,15 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'm780109022-4ac13fd210d048a7d96b8979', 	//Openwrt Router
-	'm783953686-5912db7169eea9ce488b60c1',//BlogHome
-	'm780108929-9178e0b3e091daa86ec2a7f3', 	//Aliyun HK
-	'm781833645-500dd4676d2f8f1d57199e6c', 	//Aliyun HZ
-	'm780624347-31661dfd1ec086b180801040', 	//Tencent GZ
-	'm783531673-15c8868ee165e3c80807b7c0', 	//Google TW
-	'm783953743-37e6e7168359948ba0151260',//AWSHK
-	'm783953751-fec9f51f660997d5efe77857',//Oracle Cloud
-	'm781450763-856d189f18a8095b76b8c9ae', 	//BWGhost DC6
-	'm783953731-f12e6f295ce9a2720a14d8e1',//SyUmbrella
+	'm780109022-4ac13fd210d048a7d96b8979',  // Openwrt Router
+	'm783953686-5912db7169eea9ce488b60c1',  // Blog Home Page
+	'm780108929-9178e0b3e091daa86ec2a7f3',  // Aliyun HK
+	'm781833645-500dd4676d2f8f1d57199e6c', 	// Aliyun HZ
+	'm780624347-31661dfd1ec086b180801040', 	// Tencent GZ
+	'm783531673-15c8868ee165e3c80807b7c0', 	// Google HK
+	'm783953743-37e6e7168359948ba0151260',  // AWS HK
+	'm783953751-fec9f51f660997d5efe77857',  // Oracle Cloud
+	'm781450763-856d189f18a8095b76b8c9ae', 	// BWGhost DC6
+	'm783953731-f12e6f295ce9a2720a14d8e1',  // SyUmbrella Home Page
 ];
 
 // refresh interval (in seconds)
