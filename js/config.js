6f58907b4430377b3fb22f3017ed8b1156ba2467
eallion <eallion@eallion.com>
2019-12-07 13:12:09 +0800

    delete Gridea for CoolQCup

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm780108929-9178e0b3e091daa86ec2a7f3', 	//Aliyun HK
+	'm781833645-500dd4676d2f8f1d57199e6c', 	//Aliyun HZ
+	'm780624347-31661dfd1ec086b180801040', 	//Tencent GZ
+	'm783531673-15c8868ee165e3c80807b7c0', 	//Google TW
+	'm781450763-856d189f18a8095b76b8c9ae', 	//BWGhost DC6
+	'm780109022-4ac13fd210d048a7d96b8979', 	//Openwrt Router
+];
+
+// refresh interval (in seconds)
+var __refresh = 180;
